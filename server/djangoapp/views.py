79ad2acde99e2f236c3bd6f64ab56ba18c528c0a
berketonoz <btonoz@sabanciuniv.edu>
2024-07-26 02:01:17 +0300

    seperated into functions

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,16 +1,41 @@
 import json
-from urllib.parse import urlparse
 from django.http import HttpResponse
-from django.shortcuts import render
-
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+
+def fetch_page(url, headers):
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    return response.content
+
+def parse_product(product, selectors):
+    product_name = product.find(selectors['name']['tag'], {'class': selectors['name']['class']})
+    product_currency = product.find(selectors['currency']['tag'], {'class': selectors['currency']['class']})
+    product_price_whole = product.find(selectors['price_whole']['tag'], {'class': selectors['price_whole']['class']})
+    product_price_fraction = product.find(selectors.get('price_fraction', {}).get('tag'), {'class': selectors.get('price_fraction', {}).get('class')}) if selectors.get('price_fraction') else None
+    product_stock_info = product.find(selectors['stock']['tag'], {'class': selectors['stock']['class']})
+
+    product_info = {}
+    if product_name:
+        product_info['name'] = product_name.get_text(strip=True)
+    if product_currency:
+        product_info['currency'] = product_currency.get_text(strip=True)
+    if product_price_whole:
+        product_info['price'] = product_price_whole.get_text(strip=True) + (product_price_fraction.get_text(strip=True) if product_price_fraction else '')
+    if product_stock_info:
+        product_info['stock'] = product_stock_info.get_text(strip=True)
+    else:
+        product_info['stock'] = 'In Stock'
+
+    return product_info
 
 def scrap_amazon(query):
-    protocol = 'https://www.'
-    domain = 'amazon'
-    path = '/s?k='
+    base_url = 'https://www.amazon'
     tlds = ['.ae', '.com.tr']
+    path = '/s?k='
     selectors = {
         'product': {'tag': 'div', 'attr': {'data-component-type': 's-search-result'}},
         'name': {'tag': 'span', 'class': 'a-size-base-plus a-color-base a-text-normal'},
@@ -20,7 +45,6 @@ def scrap_amazon(query):
         'stock': {'tag': 'span', 'class': 'a-size-base a-color-price'},
     }
     headers = {
-        # Cookie MUST be included in the header  
         'Cookie': '',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept': '*/*',
@@ -28,51 +52,29 @@ def scrap_amazon(query):
         'Connection': 'keep-alive',
     }
     product_info_list = []
-    
+
     for tld in tlds:
-        url = protocol + domain + tld + path + query
-        print("URL: ", url)
-        response = requests.get(url, headers=headers)
-
-        if response.status_code != 200:
-            return f"Failed to retrieve the webpage. Status code: {response.status_code}"
-
+        url = f"{base_url}{tld}{path}{query}"
         try:
-            # Extracting the product names and prices from the search results
-            soup = BeautifulSoup(response.content, 'html.parser')
+            page_content = fetch_page(url, headers)
+            soup = BeautifulSoup(page_content, 'html.parser')
             products = soup.find_all(selectors['product']['tag'], selectors['product']['attr'])
-            index = 1
-            extracted_data = []
-            for product in products:
-                product_name = product.find(selectors['name']['tag'], {'class': selectors['name']['class']})
-                product_currency = product.find(selectors['currency']['tag'], {'class': selectors['currency']['class']})
-                product_price_whole = product.find(selectors['price_whole']['tag'], {'class': selectors['price_whole']['class']})
-                product_price_fraction = product.find(selectors['price_fraction']['tag'], {'class': selectors['price_fraction']['class']})
-                product_stock_info = product.find(selectors['stock']['tag'], {'class': selectors['stock']['class']})
 
-                p = {'index': index, 'url': domain + tld }
-                if product_name:
-                    p['name'] = product_name.get_text(strip=True)
-                if product_currency:
-                    p['currency'] = product_currency.get_text(strip=True)
-                if product_price_whole:
-                    p['price'] = product_price_whole.get_text(strip=True) + (product_price_fraction.get_text(strip=True) if product_price_fraction else '')
-                if product_stock_info:
-                    p['stock'] = product_stock_info.get_text(strip=True)
-                else:
-                    p['stock'] = 'In Stock'
-                extracted_data.append(p)
-                index += 1
-            product_info_list.extend(extracted_data)
-        except AttributeError as e:
-            return f"An error occurred while parsing the webpage: {e}"
-    return product_info_list if product_info_list else "No products found."
+            for index, product in enumerate(products, start=1):
+                product_info = parse_product(product, selectors)
+                product_info['index'] = index
+                product_info['url'] = f"{base_url}{tld}"
+                product_info_list.append(product_info)
+        except Exception as e:
+            print(e)
+            return str(e)
+    
+    return product_info_list
 
 def scrap_hepsiburada(query):
-    protocol = 'https://www.'
-    domain = 'hepsiburada'
-    path = '/ara?q='
+    base_url = 'https://www.hepsiburada'
     tlds = ['.com']
+    path = '/ara?q='
     selectors = {
         'product_list': {'tag': 'ul', 'class': 'productListContent'},
         'product': {'tag': 'li', 'class': 'productListContent-frGrtf5XrVXRwJ05HUfU'},
@@ -89,59 +91,52 @@ def scrap_hepsiburada(query):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
     product_info_list = []
-    
+
     for tld in tlds:
-        url = protocol + domain + tld + path + query
-        response = requests.get(url, headers=headers)
-
-        if response.status_code != 200:
-            return f"Failed to retrieve the webpage. Status code: {response.status_code}"
-
+        url = f"{base_url}{tld}{path}{query}"
         try:
-            # Extracting the product names and prices from the search results
-            soup = BeautifulSoup(response.content, 'html.parser')
+            page_content = fetch_page(url, headers)
+            soup = BeautifulSoup(page_content, 'html.parser')
             product_list = soup.find(selectors['product_list']['tag'], class_=lambda c: c and selectors['product_list']['class'] in c)
+            
             if product_list:
                 products = product_list.find_all(selectors['product']['tag'], class_=selectors['product']['class'])
             else:
                 products = []
-            index = 1
-            extracted_data = []
-            for product in products:
-                product_name = product.find(selectors['name']['tag'], {'class': selectors['name']['class']})
-                product_currency = product.find(selectors['currency']['tag'], {'class': selectors['currency']['class']})
-                product_price_whole = product.find(selectors['price_whole']['tag'], {'class': selectors['price_whole']['class']})
-                product_stock_info = product.find(selectors['stock']['tag'], {'class': selectors['stock']['class']})
-                
-                p = {'index': index}
-                if product_name:
-                    p['name'] = product_name.get_text(strip=True)
-                if product_currency:
-                    p['currency'] = product_currency.get_text(strip=True)
-                if product_price_whole:
-                    p['price'] = product_price_whole.get_text(strip=True)
-                if product_stock_info:
-                    p['stock'] = product_stock_info.get_text(strip=True)
-                else:
-                    p['stock'] = 'In Stock'
-                extracted_data.append(p)
-                index += 1
-            print(f'{query.capitalize()} in {domain+tld} has {len(extracted_data)} different products')
-            product_info_list.extend(extracted_data)
-        except AttributeError as e:
-            return f"An error occurred while parsing the webpage: {e}"
-    return product_info_list if product_info_list else "No products found."
 
+            for index, product in enumerate(products, start=1):
+                product_info = parse_product(product, selectors)
+                product_info['index'] = index
+                product_info['url'] = f"{base_url}{tld}"
+                product_info_list.append(product_info)
+        except Exception as e:
+            print(e)
+            return str(e)
+    
+    return product_info_list
 
-# Create your views here.
 def home(request):
-    # Products declaration
     products = {}
-
-    # Example usage
     query = 'razer'
     products['amazon'] = scrap_amazon(query)
-    # products['hepsiburada'] = scrap_hepsiburada(query)
+    products['hepsiburada'] = scrap_hepsiburada(query)
+    
     print(products)
-    return HttpResponse(json.dumps(products),
-                    content_type='application/json; charset=utf8')
+    return HttpResponse(json.dumps(products), content_type='application/json; charset=utf8')
+
+def test_connection(request):
+    # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/')
+
+    # Access a specific database
+    db = client['mydatabase']
+
+    # Access a specific collection
+    collection = db['mycollection']
+
+    # Insert a document
+    result = collection.insert_one({'name': 'Razer Product', 'type': 'Mouse'})
+
+    # Find a document
+    document = collection.find_one({'name': 'Razer Product'})
+    print(document)
