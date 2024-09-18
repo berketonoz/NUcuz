78406288384720250a967553c57b2ea8a8147ea0
berketonoz <btonoz@sabanciuniv.edu>
2024-09-19 00:15:53 +0300

    google login success implementation created

--- server/frontend/src/components/Login/Login.jsx
+++ server/frontend/src/components/Login/Login.jsx
@@ -25,13 +25,38 @@ const Login = ({ onClose }) => {
   const [open, setOpen] = useState(true);
 
   const login_url = "http://localhost:8000/djangoapp/login"; // Your Django login API URL
+  const login_url_google = "http://localhost:8000/djangoapp/accounts/google/login/token/"; // Your Django login API URL for Google
+
+  const handleGoogleSuccess = (response) => {
+    // Send the ID token to your Django backend
+    fetch(login_url_google, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: response.credential,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "Authenticated") {
+          console.log("Login success:", data);
+          // Handle success (store token, redirect, etc.)
+          sessionStorage.setItem("username", data.username);
+          sessionStorage.setItem("screen_name", data.screen_name);
+          window.location.href = "/";
+        }
+      })
+      .catch((error) => {
+        console.error("Error logging in:", error);
+      });
+  };
 
   // Traditional login handler
   const login = async (e) => {
     e.preventDefault();
-
     const csrftoken = getCookie("csrftoken");
-
     const res = await fetch(login_url, {
       method: "POST",
       headers: {
@@ -109,9 +134,7 @@ const Login = ({ onClose }) => {
             <div className="google-login">
               <GoogleLogin
                 text="signin_with"
-                onSuccess={() => {
-                  console.log("Success");
-                }}
+                onSuccess={handleGoogleSuccess}
                 onError={() => {
                   console.log("Error");
                 }}
