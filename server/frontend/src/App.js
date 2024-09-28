1d6d8706eceff87efe57e8fb074055c22df7b308
berketonoz <btonoz@sabanciuniv.edu>
2024-09-28 23:04:39 +0300

    Router place changed

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,7 @@
 import Products from "./components/Products/Products";
 import Product from "./components/Products/Product";
 import Home from "./components/Home/Home";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 export default App;
