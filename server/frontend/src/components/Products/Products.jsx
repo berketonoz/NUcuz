f86bad365343883bbda89fced37d3fdc56a88a37
berketonoz <btonoz@sabanciuniv.edu>
2024-09-28 23:03:30 +0300

    filters changed with accordion component

--- server/frontend/src/components/Products/Products.jsx
+++ server/frontend/src/components/Products/Products.jsx
@@ -1,46 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "./Products.css";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import { Box, FormControl } from "@mui/material";
 
 const Products = () => {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [productsList, setProductsList] = useState([]);
   const [error, setError] = useState(null);
   const [currencies, setCurrencies] = useState([]);
-  const [currency, setCurrency] = useState([]);
-  const [rating, setRating] = useState([]);
-  const [filters, setFilters] = useState({
-    currency: '',
-    product_star_rating: '',
-  })
-  
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
+  const [checkedState, setCheckedState] = useState({
+    currency: {},
+    rating: {
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      5: false,
     },
-  },
-};
+  });
+  const ratings = [1, 2, 3, 4, 5];
 
   const products_url = "http://localhost:8000/djangoapp/products";
-
-  const toggle = (e) => {
-    let e1 = document.getElementsByClassName("arrow");
-    e1 = e1[0];
-    e1.classList.toggle("active");
-    e1.style.transition = "transform 0.2s ease";
-    if (e1.classList.contains("active")) e1.style.transform = "rotate(45deg)";
-    else e1.style.transform = "rotate(-45deg)";
-    console.log(currencies);
-  };
 
   const getProducts = async () => {
     try {
@@ -52,9 +40,8 @@ const MenuProps = {
             let currencies = Array.from(
               new Set(data.products.map((product) => product.currency))
             );
-            console.log(currencies);
-            setCurrencies(currencies);
-            console.log('products',data.products)
+            // setCurrencies(currencies);
+            setCurrencies(["TRY", "USD"]);
             setProductsList(data.products);
             setFilteredProducts(data.products);
           } else {
@@ -65,6 +52,53 @@ const MenuProps = {
       console.error("Error fetching products: ", err);
       setError("Error fetching products");
     }
+  };
+
+  const handleFilter = () => {
+    const { currency, rating } = checkedState;
+
+    const newFilteredProducts = productsList.filter((product) => {
+      // Check if the product's currency is checked
+      const matchesCurrency = currency[product.currency] === true;
+      // Check if the product's currency is checked
+      const matchesRating = rating[Math.round(Number(product.product_star_rating))] === true;
+
+      // If no filters are applied, return true for all products
+      return (
+        (
+          !Object.values(currency).some((value) => value === true) ||
+          matchesCurrency ) && 
+        (
+          !Object.values(rating).some((value) => value === true) ||
+          matchesRating
+        )
+      );
+    });
+    setFilteredProducts(newFilteredProducts);
+  };
+
+  const handleCheckboxCurrency = (event) => {
+    const { name, checked } = event.target;
+    setCheckedState((prev) => ({
+      ...prev,
+      currency: {
+        ...prev.currency,
+        [name]: checked,
+      },
+    }));
+    // handleFilter(); // Call filter after updating state
+  };
+
+  const handleCheckboxRating = (event) => {
+    const { name, checked } = event.target;
+    setCheckedState((prev) => ({
+      ...prev,
+      rating: {
+        ...prev.rating,
+        [name]: checked,
+      },
+    }));
+    // handleFilter(); // Call filter after updating state
   };
 
   const searchHandle = async (input) => {
@@ -83,82 +117,72 @@ const MenuProps = {
     getProducts();
   }, []);
 
+  // UseEffect to handle filtering when `checkedState` changes
+  useEffect(() => {
+    handleFilter();
+  }, [checkedState]);
+
   const isLoggedIn = sessionStorage.getItem("username") !== null;
-
-  const handleChangeCurrency = (event) => {
-    console.log(event.target);
-    
-    console.log('name :', event.target.name, 'value :', event.target.value);
-
-    setCurrency(event.target.value);
-    if(event.target.value !== "") {
-        var productsByCurrency = productsList.filter((item) => item.currency === event.target.value);
-        setFilteredProducts(productsByCurrency);
-    } else {
-        setFilteredProducts(productsList);
-    }
-  };
-
-  const handleChangeRating = (e) => {
-    setRating(e.target.value);
-    console.log(Number(rating));
-    var filteredList = productsList.filter((item) => Number(item.product_star_rating) <= Number(e.target.value) );
-    setFilteredProducts(filteredList);
-  };
 
   return (
     <div>
       {error && <div className="error">{error}</div>}
       <div className="container-ver">
         <div className="filter-container">
-            <p className="filter-header">Filters</p>
-            <div className="dropdown-container">
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label-currency">Currency</InputLabel>
-                        <Select
-                        labelId="demo-simple-select-label-currency"
-                        id="demo-simple-select-currency"
-                        value={currency}
-                        label="Currency"
-                        name="currency"
-                        onChange={handleChangeCurrency}
-                        >
-                            <MenuItem key="empty" value="">Seçiniz</MenuItem>  
-                            {currencies.map((c) => {
-                                return (
-                                    <MenuItem key={c} value={c}>{c}</MenuItem>
-                                )
-                            })}
-                            <MenuItem key="usd" value="USD">USD</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Box>
-                
-            </div>
-            <div className="dropdown-container">
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label-rating">Rating</InputLabel>
-                        <Select
-                        labelId="demo-simple-select-label-rating"
-                        id="demo-simple-select-rating"
-                        value={rating}
-                        label="Rating"
-                        name="product_star_rating"
-                        onChange={handleChangeRating}
-                        >
-                            <MenuItem key="empty" value="">Seçiniz</MenuItem>  
-                            <MenuItem key="1" value="1">0-1</MenuItem>
-                            <MenuItem key="2" value="2">1-2</MenuItem>
-                            <MenuItem key="3" value="3">2-3</MenuItem>
-                            <MenuItem key="4" value="4">3-4</MenuItem>
-                            <MenuItem key="5" value="5">4-5</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Box>
-                
-            </div>
+          <p className="filter-header">Filters</p>
+          <div className="">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Currency</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {currencies.map((c, index) => (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={checkedState.currency[c] || false}
+                        onClick={handleCheckboxCurrency}
+                        name={c}
+                        key={index}
+                      />
+                    }
+                    key={index}
+                    label={c}
+                  />
+                ))}
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Rating</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {ratings.map((r, index) => (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={checkedState.rating[r] || false}
+                        onChange={handleCheckboxRating}
+                        name={r.toString()}
+                        key={index}
+                      />
+                    }
+                    key={index}
+                    label={r}
+                  />
+                ))}
+              </AccordionDetails>
+            </Accordion>
+          </div>
         </div>
         <div className="container-hor">
           <div className="search-container">
@@ -171,9 +195,12 @@ const MenuProps = {
           <div className="cards-container">
             {filteredProducts.map((product, index) => (
               <div key={`${product.asin}-${index}`} className="card">
-              <Link to={`/product/${product.asin}`}>
-                <img src={product.product_photo} alt={product.product_title} />
-              </Link>
+                <Link to={`/product/${product.asin}`}>
+                  <img
+                    src={product.product_photo}
+                    alt={product.product_title}
+                  />
+                </Link>
                 <div className="card-content">
                   <p className="card-asin">{product.asin}</p>
                   <h3 className="card-title">{product.product_title}</h3>
